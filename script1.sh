39f1174e82dd013ae0e91692a12855781db1f85c
raksham24bsa10217-max <raksham.24bsa10217@vitbhopal.ac.in>
2026-03-31 00:26:31 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Raksham Sharma | Course: Open Source Software
 
+# --- Variables ---
+STUDENT_NAME="Raksham Sharma"
+SOFTWARE_CHOICE="VLC Media Player"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2)
+
+# --- Display ---
+echo "================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "================================"
+echo "Software : $SOFTWARE_CHOICE"
+echo "Kernel   : $KERNEL"
+echo "User     : $USER_NAME"
+echo "Uptime   : $UPTIME"
+echo "Date     : $DATE"
+echo "Distro   : $DISTRO"
+echo ""
+echo "License  : Linux is mostly covered under GPL (General Public License)"
