9fab9b000404e44c760b99aad5ed8ff5d739cd90
raksham24bsa10217-max <raksham.24bsa10217@vitbhopal.ac.in>
2026-03-31 16:59:21 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+# Script 5: User & Process Monitor
 
+echo "=============================="
+echo " User & Process Monitor"
+echo "=============================="
+
+# Current user
+echo "Current User:"
+whoami
+
+echo ""
+
+# Logged in users
+echo "Logged in Users:"
+who
+
+echo ""
+
+# Top processes by CPU usage
+echo "Top 5 Processes (CPU usage):"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -6
